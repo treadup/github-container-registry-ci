5971425a6b28822f00fc96b066eeb128b743c76c
Henrik Treadup <henrik.treadup@gmail.com>
2021-10-09 21:27:51 +0200

    Use multiple files in hash calculation
    Calculate the hash used for versioning the dependency docker image
    based on the following three files.
    
    1. requirements.txt
    2. dependencies.dockerfile
    3. scripts/build_docker_image.sh
    
    Any changes to the above file will trigger a rebuild of the dependency docker image.

--- scripts/filehash.py
+++ scripts/filehash.py
@@ -1,21 +1,32 @@
 """
-Computes the sha1 hash of the requirements.txt file. This hash should
-be used
+Computes the sha1 hash of the content of the following files
+concatenated together.
+1. requirements.txt
+2. dependencies.dockerfile
+3. scripts/build_docker_image.sh
 """
 import hashlib
 
-def read_file():
+def read_file(filename):
     """
-    Read requirements file as binary.
+    Read contents of file.
     """
-    with open("requirements.txt", "r") as f:
-        return f.read().encode('utf-8')
+    with open(filename, "r") as f:
+        return f.read()
 
-def print_hash(data):
+def read_all_files():
+    content = [
+        read_file("requirements.txt"),
+        read_file("dependencies.dockerfile"),
+        read_file("scripts/build_docker_image.sh")
+    ]
+    return "\n".join(content)
+
+def print_hash(text):
     """
     Print the sha1 hash of the data.
     """
-    print(hashlib.sha1(data).hexdigest())
+    print(hashlib.sha1(text.encode('utf-8')).hexdigest())
 
-data = read_file()
-print_hash(data)
+text = read_all_files()
+print_hash(text)

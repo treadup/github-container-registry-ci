04927b094b4b60eaac15725302969234820c6312
Henrik Treadup <henrik.treadup@gmail.com>
2021-10-10 13:57:27 +0200

    Use different file lists in filehash program depending on environment
    python filehash.py local will hash the files that building the local dependencies docker
    image depends on.
    python filehash.py ci will hash the files that building the ci dependencies docker image
    depends on.

--- scripts/filehash.py
+++ scripts/filehash.py
@@ -6,6 +6,7 @@ concatenated together.
 3. scripts/build_docker_image.sh
 """
 import hashlib
+import sys
 
 def read_file(filename):
     """
@@ -14,19 +15,59 @@ def read_file(filename):
     with open(filename, "r") as f:
         return f.read()
 
+def files_for_environment():
+    """
+    Returns a list the files that should checked
+    for the given environment.
+    The different environments use different files
+    to build the docker images.
+    The file hash should only change when one of
+    these files changes.
+    """
+    environment = sys.argv[1]
+    if environment == "local":
+        return [
+            "requirements.txt",
+            "dependencies.dockerfile",
+            "scripts/build_docker_image.sh"
+        ]
+    elif environment == "ci":
+        return [
+            "requirements.txt",
+            "dependencies.dockerfile",
+            "scripts/build_docker_image_ci.sh"
+        ]
+
 def read_all_files():
-    content = [
-        read_file("requirements.txt"),
-        read_file("dependencies.dockerfile"),
-        read_file("scripts/build_docker_image.sh")
-    ]
+    """
+    Read the content of all the files that should be
+    checked for the given environment and returns their
+    concatenated content.
+    """
+    files = files_for_environment()
+    content = [read_file(f) for f in files]
     return "\n".join(content)
 
 def print_hash(text):
     """
-    Print the sha1 hash of the data.
+    Print the sha1 hash of the text.
     """
     print(hashlib.sha1(text.encode('utf-8')).hexdigest())
+
+def check_usage():
+    """
+    Check that the usage is correct.
+    """
+    if len(sys.argv) != 2:
+        print("Usage: python3 filehash <environment>")
+        print("where <environment> can be 'local' or 'ci'.")
+        sys.exit(1)
+    environment = sys.argv[1]
+    if environment not in ("local", "ci"):
+        print("You need to specify 'local' or 'ci' as the environment.")
+        sys.exit(1)
+
+check_usage()
 
 text = read_all_files()
 print_hash(text)
